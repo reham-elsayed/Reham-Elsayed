aeef34cbb8921e1240ee1ad731ce3f0f98679da2
reham-elsayed <rehamshipl445@gmail.com>
2025-09-02 14:43:07 +0300

    lenis for scroll

--- app/Components/AnimatedClipContainer/AnimatedClipContainer.tsx
+++ app/Components/AnimatedClipContainer/AnimatedClipContainer.tsx
@@ -1,36 +1,47 @@
 "use client"
 
-import { motion, useAnimation } from "framer-motion"
+import { motion, scale, useAnimate, useAnimation } from "framer-motion"
 import { useEffect, useState } from "react"
 import { useInView } from "react-intersection-observer"
-
+import lenis from "lenis"
 const AnimatedClipContainer = () => {
-    const controls = useAnimation()
+    const [scope, animate] = useAnimate()
     const { ref, inView } = useInView({
-        threshold: 0.5, // trigger when 50% of section is visible
+        threshold: 0.1,
         triggerOnce: true
 
+
     })
-    const [locked, setLocked] = useState(false)
 
     useEffect(() => {
-        const runAnimation = async () => {
-            if (inView && !locked) {
-                setLocked(true)
-                document.body.style.overflow = "hidden"
-
-                await controls.start({
-                    scale: 1.5,
-                    height: 50,
-                    transition: { duration: 3, repeat: 0 },
-                })
-
-                document.body.style.overflow = "auto"
-                setLocked(false)
-            }
+        const lenisInstance = new lenis({
+            duration: 1.2,
+        })
+        console.log(lenisInstance)
+        function raf(time: number) {
+            lenisInstance.raf(time)
+            requestAnimationFrame(raf)
         }
-        runAnimation()
-    }, [inView, locked, controls])
+        requestAnimationFrame(raf)
+
+        return () => {
+            lenisInstance.destroy()
+        }
+    }, [])
+    async function handleAnimate() {
+        if (lenisInstance?.options) {
+            lenisInstance.options.duration = 20
+        }
+        await animate(scope.current, { scale: 2 }, { duration: 1 })
+
+    }
+
+    useEffect(() => {
+
+        if (inView) {
+            handleAnimate()
+        }
+    }, [inView])
 
     return (
         <div className="h-[200vh]">
@@ -38,13 +49,26 @@ const AnimatedClipContainer = () => {
 
                 className="h-screen flex items-center justify-center bg-gray-100"
             >
-                <motion.div
-                    initial={{ scale: 1, rotate: 0 }}
-                    animate={controls}
-                    className="w-40 h-5 bg-blue-500 rounded-lg"
-                />
+
+                <div ref={ref} className="h-56">
+
+                    <motion.div
+                        animate={{ y: inView ? "-100%" : " 0" }}
+                        transition={{ duration: 5 }}
+                        className="w-56 h-56 bg-blue-500 rounded-lg shadow-lg flex items-center justify-center text-white text-2xl font-bold"
+                    >
+                        Animated Box
+                    </motion.div>
+                    <motion.div
+                        animate={{ y: inView ? "100%" : " 0" }}
+                        transition={{ duration: 5 }}
+                        className="w-56 h-56 bg-blue-500 rounded-lg shadow-lg flex items-center justify-center text-white text-2xl font-bold"
+                    >
+                        Animated Box
+                    </motion.div>
+
+                </div>
             </div>
-            <div ref={ref} className="h-56"></div>
         </div>
     )
 }
