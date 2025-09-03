651e52bcb462e609f97a1dd81667abcdc92908e2
reham-elsayed <rehamshipl445@gmail.com>
2025-09-03 12:31:43 +0300

    height animation

--- app/Components/AnimatedClipContainer/AnimatedClipContainer.tsx
+++ app/Components/AnimatedClipContainer/AnimatedClipContainer.tsx
@@ -1,18 +1,18 @@
 "use client"
 
 import { motion, useAnimate, useScroll, useTransform } from "framer-motion"
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import { useInView } from "react-intersection-observer"
 import Lenis from "lenis"
 
 const AnimatedClipContainer = () => {
-    const [scope, animate] = useAnimate()
-    const { ref, inView } = useInView({
-        threshold: 0.1,
-        triggerOnce: true,
-    })
+    // const { ref, inView } = useInView({
+    //     threshold: 0.1,
+    //     triggerOnce: true,
+    // })
+    const ref = useRef<HTMLDivElement>(null)
     const { scrollYProgress } = useScroll()
-    const scrollY = useTransform(scrollYProgress, [0, 1], [0, 100])
+    const scrollY = useTransform(scrollYProgress, [0, 1], [5, 100])
     // ✅ Lenis stored in a ref
     const lenisRef = React.useRef<Lenis | null>(null)
 
@@ -32,36 +32,45 @@ const AnimatedClipContainer = () => {
         }
     }, [])
 
-    async function handleAnimate() {
-        if (lenisRef.current) {
-            // ✅ now you can modify lenis safely
-            lenisRef.current.options.duration = 5
-        }
+    // async function handleAnimate() {
+    //     if (lenisRef.current) {
+    //         // ✅ now you can modify lenis safely
+    //         lenisRef.current.options.duration = 5
+    //     }
 
-        await animate(scope.current, { scale: 2 }, { duration: 1 })
-    }
+    //     await animate(scope.current, { scale: 2 }, { duration: 1 })
+    // }
 
-    useEffect(() => {
-        if (inView) {
-            handleAnimate()
-        }
-    }, [inView])
+    // useEffect(() => {
+    //     if (inView) {
+    //         handleAnimate()
+    //     }
+    // }, [inView])
 
     return (
-        <div className="h-[200vh]">
-            <div className="h-screen flex items-center justify-center bg-gray-100">
-                <div ref={ref} className="h-56" >
+        <div className="h-[200vh] relative">
+            <div className="h-screen flex items-center justify-center bg-gray-100  sticky inset-0.5">
+                <div ref={ref} className="h-56 flex flex-col items-center justify-center " >
                     <motion.div
-                        initial={{ y: "0" }}
-                        style={{ y: -scrollY }}
+                        initial={{ y: "5" }}
+
                         transition={{ duration: 5 }}
                         className="w-56 h-56 bg-blue-500 rounded-lg shadow-lg flex items-center justify-center text-white text-2xl font-bold"
                     >
                         Animated Box
                     </motion.div>
                     <motion.div
+                        initial={{ height: "5px" }}
+                        style={{ height: scrollY }}
+                        transition={{ duration: 5 }}
+                        className="overflow-hidden flex items-center justify-center bg-amber-400"
+                    >
+                        <h2 className="text-3xl font-bold mt-4">Scroll to Animate</h2>
+
+                    </motion.div>
+                    <motion.div
                         initial={{ y: "0" }}
-                        style={{ y: scrollY }}
+
                         transition={{ duration: 5 }}
                         className="w-56 h-56 bg-blue-500 rounded-lg shadow-lg flex items-center justify-center text-white text-2xl font-bold"
                     >
