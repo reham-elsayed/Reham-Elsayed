5418112bf0af413ec3d568fac64781ecb672bfd5
reham-elsayed <rehamshipl445@gmail.com>
2025-09-15 01:39:06 +0300

    hero animation

--- app/Components/AnimatedClipContainer/AnimatedClipContainer.tsx
+++ app/Components/AnimatedClipContainer/AnimatedClipContainer.tsx
@@ -7,36 +7,73 @@ import TextHeroAnimated from "../TextHeroAnimated/TestHeroAnimated"
 const AnimatedClipContainer = () => {
 
     const ref = useRef<HTMLDivElement>(null)
-    const { scrollYProgress } = useScroll(
-        {
-            target: ref,
-            offset: ["start end", "end end"],
-
-        }
-    )
+    const { scrollYProgress } = useScroll()
     const maxHeight = typeof window !== "undefined" ? window.innerHeight * 0.5 : 400
-    const scrollY1 = useTransform(scrollYProgress, [0, 0.2], [10, maxHeight])
-    const scaleX = useTransform(scrollYProgress, [0, .5], [1, 1.1])
+    const height = useTransform(scrollYProgress, [0, .5], ["1%", "100%"])
+    const width = useTransform(scrollYProgress, [0, .5], ["85%", "100%"])
+
+
+    const y = useTransform(scrollYProgress, [.2, .7], [0, 150]);//translate down
+    const invertedY = useTransform(y, (v) => -v);
+    const weight = useTransform(scrollYProgress, [0, 2, .7], [300, 700, 300]); // font weight
+    const widthT = useTransform(scrollYProgress, [0, .2, .7], [160, 100, 85]); // font width
+    const fontSize = useTransform(scrollYProgress, [0, .5], ["2.5rem", "1.8rem"]); // font size
 
     return (
-        <div className="bg-gray-100 ">
-            <div className="relative  h-[200vh] container mx-auto">
-                <div className="   sticky inset-0 w-full overflow-hidden ">
-                    <div ref={ref} className="h-full  flex flex-col items-center justify-center " >
-                        <TextHeroAnimated text="REHAM ELSAYED" className="z-10" moveDown={true} />
-                        <TextHeroAnimated text="FRONTEND DEVELOPER" className="z-20" moveDown={true} />
+        <div className="bg-gray-100  ">
+            <div className="relative  h-[300vh] container mx-auto">
+                <div className="   sticky left-0 right-0 -top-7 w-full h-screen overflow-hidden flex items-stretch justify-center">
+                    <div className="overflow-hidden relative flex flex-col flex-1 items-center self-stretch justify-center " >
+                        <div
+
+                            className={` bg-red-100  w-full wrapper-h1 `}>
+                            {/* Big scroll space so you can see effect */}
+                            <motion.div
+
+                                style={{
+                                    y,
+                                    fontVariationSettings: `"wght" ${weight}, "wdth" ${widthT}`,
+                                    fontFamily: "Geist Variable, sans-serif", // must be a variable font
+                                    fontSize: fontSize,
+                                    transformStyle: "preserve-3d",
+                                }}
+                                transition={{ duration: 10 }}
+                                className="text-center hero-txt"
+                            >
+                                reham elsayed
+                            </motion.div>
+                        </div>
+
+                        <div
+
+                            className={` bg-red-100  w-full wrapper-h1 `}>
+                            {/* Big scroll space so you can see effect */}
+                            <motion.div
+
+                                style={{
+                                    y,
+                                    fontVariationSettings: `"wght" ${weight}, "wdth" ${widthT}`,
+                                    fontFamily: "Geist Variable, sans-serif", // must be a variable font
+                                    fontSize: fontSize,
+                                    transformStyle: "preserve-3d",
+                                }}
+                                transition={{ duration: 10 }}
+                                className="text-center hero-txt"
+                            >
+                                reham elsayed
+                            </motion.div>
+                        </div>
                         <motion.div
-                            style={{ height: scrollY1, scaleX: scaleX }}
+                            style={{ height, width }}
                             transition={{ duration: 10 }}
                             id="clip-container"
                             className=" flex items-center justify-center bg-amber-400 z-50 w-[80vw] mx-auto"
                         >
                             <motion.h2
-                                className="text-3xl font-bold mt-4 ">Scroll to Animate</motion.h2>
+                                className="text-3xl font-bold mt-4 "></motion.h2>
 
                         </motion.div>
-                        <TextHeroAnimated text="REHAM ELSAYED" moveDown={false} className="z-20" />
-                        <TextHeroAnimated text="FRONTEND DEVELOPER" moveDown={false} className="z-10" />
+
                     </div>
                 </div>
             </div>
--- app/Components/TextHeroAnimated/TestHeroAnimated.tsx
+++ app/Components/TextHeroAnimated/TestHeroAnimated.tsx
@@ -19,30 +19,30 @@ export default function ScrollAnimatedText({ text, className, moveDown }: Props)
     )
 
     // Map scroll progress → animation values
-    const y = useTransform(scrollYProgress, [.5, 1], [0, 150]);//translate down
+    const y = useTransform(scrollYProgress, [0, 1], [100, 150]);//translate down
     const invertedY = useTransform(y, (v) => -v);
     const weight = useTransform(scrollYProgress, [0, 1, 1.5], [300, 700, 300]); // font weight
     const width = useTransform(scrollYProgress, [0, 1, 1.5], [160, 100, 85]); // font width
     const fontSize = useTransform(scrollYProgress, [0, 1], ["2.5rem", "1.8rem"]); // font size
     return (
-        <section
+        <div
 
-            className={`${className} bg-gray-100 `}>
+            className={`${className} bg-red-100  w-full wrapper-h1 `}>
             {/* Big scroll space so you can see effect */}
             <motion.div
-                ref={ref}
+
                 style={{
-                    y: moveDown ? y : invertedY,
+                    y,
                     fontVariationSettings: `"wght" ${weight}, "wdth" ${width}`,
                     fontFamily: "Geist Variable, sans-serif", // must be a variable font
                     fontSize: fontSize,
                     transformStyle: "preserve-3d",
                 }}
                 transition={{ duration: 10 }}
-                className="text-center"
+                className="text-center hero-txt"
             >
                 {text}
             </motion.div>
-        </section>
+        </div>
     );
 }
