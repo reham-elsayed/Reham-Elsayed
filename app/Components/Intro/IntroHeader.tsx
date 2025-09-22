f5b519cf634222e7e7bd82bba9dee4b042a46da7
reham-elsayed <rehamshipl445@gmail.com>
2025-09-22 16:50:23 +0300

    introanimation stack

---
+++ app/Components/Intro/IntroHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const IntroHeader = () => {
+    return (
+        <div className='relative flex justify-center items-center w-full h-dvh bg-yellow-600'>
+            <div className='h-full w-[90vw] mx-auto absolute inset-0 flex justify-center items-center'>
+                <div className='h-[20vw] w-full flex bg-gray-200'>
+                    <div className='w-[90vw]'> front end reham elsayed</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default IntroHeader
--- app/Components/Intro/IntroWrapper.tsx
+++ app/Components/Intro/IntroWrapper.tsx
@@ -3,31 +3,45 @@ import React, { useEffect } from 'react'
 import { useLenis } from '../LenisSetup/LenisSetup'
 import FolderCoverComponent from '../FolderCoverComponent/FolderCoverComponent'
 import { motion, useAnimate } from 'framer-motion'
+import IntroHeader from './IntroHeader'
 const IntroWrapper = () => {
 
     const { stop, start } = useLenis()
     const [scope, animate] = useAnimate()
-    // useEffect(() => {
-    //     async function handleOnLoadAnimation() {
-    //         await animate(scope.current, { translateY: 1500, opacity: .5 }, { duration: 5 })
-    //     }
-    //     handleOnLoadAnimation()
-    // }, [animate])
-
+    const [scope1, animate1] = useAnimate()
     useEffect(() => {
-        stop()
-        const timer = setTimeout(() => start(), 50000)
-        return () => clearTimeout(timer)
-    }, [stop, start])
+        async function handleOnLoadAnimation() {
+            stop()
+            await animate(scope.current, { translateY: 1500, opacity: .5 }, { duration: 5 })
+            await animate1(scope1.current, { translateY: -1500, rotate: "-5deg", opacity: .5 }, { duration: 5 })
 
-    return (<div className='relative overflow-hidden'>
+        }
+        handleOnLoadAnimation()
+    }, [animate])
 
+    // useEffect(() => {
+    //     stop()
+    //     const timer = setTimeout(() => start(), 50000)
+    //     return () => clearTimeout(timer)
+    // }, [stop, start])
+
+    return (<div className='relative overflow-hidden h-screen w-full'>
+        <motion.div
+            ref={scope1}
+            className='z-10 absolute inset-0 bg-black'
+        >
+
+        </motion.div>
         <motion.div
             ref={scope}
-            className=''
+            className='z-40 absolute inset-0'
         >
             <FolderCoverComponent />
         </motion.div>
+
+        <div className='absolute inset-0 -z-40'>
+            <IntroHeader />
+        </div>
     </div>)
 
 
